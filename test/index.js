a5be22334cdb39c3bae751753125068a3d2b0615
Tevel <119736325+Tadataka-Rei@users.noreply.github.com>
2026-01-08 18:06:11 +0700

    renamed:    index.js -> test/index.js 	renamed:    test.csv -> test/test.csv 	renamed:    test.rest -> test/test.rest

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const connectDB = require('./config/db');
-const User = require('./models/users');
+const connectDB = require('../config/db');
+const User = require('../models/users');
 
 const app = express();
 const PORT = 3000;
